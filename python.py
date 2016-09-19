d7f93f88bf955c35d94bd87e76ce584df5783bd8
Lupenceaa <alexandru.lupencea@dandsltd.com>
2016-09-19 22:31:29 +0300

    Adds 3 numbers now.

--- python.py
+++ python.py
@@ -1,8 +1,9 @@
 #/usr/bin/python
-# Adds two numbers.
+# Adds 3 numbers.
 print("test")
 print("hello world")
 num1 = 2
 num2 = 3
-sun = num1 + num2
-print "Sum is:" , sun
+num3 = 5
+sum = num1 + num2 + num3
+print "Sum is:" , sum
